680b244e1ca34ef122794978fe4bc64af671eea3
Julien2L <julien.lehoussine-labatut@epitech.eu>
2022-06-15 11:36:27 +0200

    Secure connection w/ .env

--- express/connection.js
+++ express/connection.js
@@ -1,11 +1,14 @@
 const mysql = require('mysql');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './.env' });
 
 var mySqlConnection = mysql.createConnection({
-    host : 'localhost',
-    user : 'root',
-    password : 'root',
-    port : 8889,
-    database : 'cinema',
+    host : process.env.DB_HOST,
+    user : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    port : process.env.DB_PORT,
+    database : process.env.DB_NAME,
     multipleStatement : true
 });
 
